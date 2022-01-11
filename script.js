68de159ee4c1cfaf22f1e96df3b628ed8d3dd119
Kyle <whatthefetch@gmail.com>
2022-01-11 13:51:39 -0800

    added html file and javascript file. Wrote pseudo code for .js file

---
+++ script.js
@@ -0,0 +1,21 @@
+//PART 1
+//prompt user to input their selection (rock paper or scissors)
+        //needs to be case-insensitive
+        //store input into some variable
+//get computer generated value using function computerPlay()
+//compare playerSelection to computerSelection
+        //if selections are the same return a message saying so and prompting for another selection else
+        //if playerSelection is paper and computerSelection is rock, return you win else
+        //if playerSelection is paper and computer Selection is scissors, rturn you lose else
+        //if playerSelection is rock and computerSelection is paper, return you lose else
+        //if playerSelection is rock and computerSeleciton is scissors, return you win else
+        //if playeSelection is scissor and computerSelection is rock, return you lose else
+        //if playerSelection is scissor and computerSelection is paper, return you win else
+        //return you didnt choose a valid selection, please retry
+//PART 2
+//use function game() to play 5 round game that keeps track of score, displays the results at the end of each round and the winner at the end of the game
+//starts a counter at 0, run round function previously created
+//return computer vs user score of rounds won
+//increase counter by 1
+//while counter is less than 5 or player score===3 or computer score ===3, do another round
+//print winner of game, whichever is greater: player score or computer score
